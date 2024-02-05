481b4316bde490eeaaf55ad833a2a90589ad0a49
Archana Berry OPS!!! <157275787+archanaberry@users.noreply.github.com>
2024-02-05 21:50:55 +0700

    Create index.hpp

---
+++ archanaberry-SignalGroup/index.hpp
@@ -0,0 +1,48 @@
+#ifndef GROUP_SESSION_BUILDER_HPP
+#define GROUP_SESSION_BUILDER_HPP
+
+class GroupSessionBuilder {
+    // Class definition goes here
+};
+
+#endif // GROUP_SESSION_BUILDER_HPP
+
+// sender_key_distribution_message.hpp
+#ifndef SENDER_KEY_DISTRIBUTION_MESSAGE_HPP
+#define SENDER_KEY_DISTRIBUTION_MESSAGE_HPP
+
+class SenderKeyDistributionMessage {
+    // Class definition goes here
+};
+
+#endif // SENDER_KEY_DISTRIBUTION_MESSAGE_HPP
+
+// sender_key_record.hpp
+#ifndef SENDER_KEY_RECORD_HPP
+#define SENDER_KEY_RECORD_HPP
+
+class SenderKeyRecord {
+    // Class definition goes here
+};
+
+#endif // SENDER_KEY_RECORD_HPP
+
+// sender_key_name.hpp
+#ifndef SENDER_KEY_NAME_HPP
+#define SENDER_KEY_NAME_HPP
+
+class SenderKeyName {
+    // Class definition goes here
+};
+
+#endif // SENDER_KEY_NAME_HPP
+
+// group_cipher.hpp
+#ifndef GROUP_CIPHER_HPP
+#define GROUP_CIPHER_HPP
+
+class GroupCipher {
+    // Class definition goes here
+};
+
+#endif // GROUP_CIPHER_HPP

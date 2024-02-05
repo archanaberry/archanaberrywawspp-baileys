a3cb768645b4c0869bbab1c84bb262b4113132b3
Archana Berry OPS!!! <157275787+archanaberry@users.noreply.github.com>
2024-02-05 23:52:05 +0700

    Create index.hpp

---
+++ archanaberry-WhatsApp/WABinary/index.hpp
@@ -0,0 +1,44 @@
+// encode.hpp
+#ifndef ENCODE_HPP
+#define ENCODE_HPP
+
+// Content of encode module
+// ...
+
+#endif // ENCODE_HPP
+
+// decode.hpp
+#ifndef DECODE_HPP
+#define DECODE_HPP
+
+// Content of decode module
+// ...
+
+#endif // DECODE_HPP
+
+// generic-utils.hpp
+#ifndef GENERIC_UTILS_HPP
+#define GENERIC_UTILS_HPP
+
+// Content of generic-utils module
+// ...
+
+#endif // GENERIC_UTILS_HPP
+
+// jid-utils.hpp
+#ifndef JID_UTILS_HPP
+#define JID_UTILS_HPP
+
+// Content of jid-utils module
+// ...
+
+#endif // JID_UTILS_HPP
+
+// types.hpp
+#ifndef TYPES_HPP
+#define TYPES_HPP
+
+// Content of types module
+// ...
+
+#endif // TYPES_HPP
